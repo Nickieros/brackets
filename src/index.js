12404454097acdf76f9da1727d1c50ab34eeef8a
Nickieros <>
2020-10-25 17:49:01 +0200

    feat: make the task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-    /** type boolean */
-    let bResult = false;
-    return bResult;
+    while(str){
+        let bIsBracketsFound = false;
+        for(const i of bracketsConfig) {
+            let brackets = i[0] + i[1];
+            if(str.includes(brackets)){
+                str = str.replace(brackets, '');
+                bIsBracketsFound = true;
+            }
+        }
+        if(!bIsBracketsFound) break;
+    }
+    return !str;
 }
